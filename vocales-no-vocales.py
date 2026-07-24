395ee8d6953e38a5d0df6328425ed8999f1ab406
dylansaidarambulalopez-max <dylansaidarambulalopez@gmail.com>
2026-07-24 14:40:58 -0600

    Add files via upload

---
+++ vocales-no-vocales.py
@@ -0,0 +1,11 @@
+#vocales / no vocales
+while True:
+    letra = input("ingrese letra ( espacio termina): ")
+    if letra == " ":
+        break
+    letra = letra.lower()
+    if letra in "aeiou":
+        print("vocal")
+    else:
+        print("consonante")
+print("programa finalizado")
